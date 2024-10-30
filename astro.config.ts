530589f05a6c11850eea92c6f5ab31bb50c232cf
Ana Rangel <rangelana1221@gmail.com>
2024-10-29 23:29:12 -0300

    update site URL

--- astro.config.ts
+++ astro.config.ts
@@ -1,28 +1,28 @@
-import i18n from '@astrolicious/i18n';
-import playformCompress from '@playform/compress';
-import { defineConfig } from 'astro/config';
+import i18n from "@astrolicious/i18n";
+import playformCompress from "@playform/compress";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://anarangel.github.io',
+  site: "https://uxanarangel.com",
   image: {
-    domains: ['media.licdn.com', 'avatars.githubusercontent.com'],
+    domains: ["media.licdn.com", "avatars.githubusercontent.com"],
   },
   vite: {
     build: {
-      cssMinify: 'lightningcss',
+      cssMinify: "lightningcss",
     },
   },
   integrations: [
     i18n({
-      defaultLocale: 'es',
-      locales: ['es', 'en'],
+      defaultLocale: "es",
+      locales: ["es", "en"],
       pages: {
-        '/sobre-mi': {
-          en: '/about-me',
+        "/sobre-mi": {
+          en: "/about-me",
         },
-        '/planograma': {
-          en: '/planogramm',
+        "/planograma": {
+          en: "/planogramm",
         },
       },
     }),
