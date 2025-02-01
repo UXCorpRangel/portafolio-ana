72069e58f48d4d3b58db94967853a465095ab795
Felix Icaza <fx.joliett17@gmail.com>
2025-01-31 20:40:58 -0600

    refactor: reorganize component imports and remove deprecated sections

--- src/pages/planograma/_sections/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Context } from './Context.astro'
-export { default as Designs } from './Designs.astro'
-export { default as Hero } from './Hero.astro'
-export { default as Process } from './Process.astro'
