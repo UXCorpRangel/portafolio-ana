92f8a2d0caac60aae252cbd254dfcd2d4a9bb07a
Felix Icaza <fx.joliett17@gmail.com>
2025-03-03 18:37:16 -0600

    feat: add carousel images for JSConf Chile event

---
+++ src/data/carousel-images.ts
@@ -0,0 +1,16 @@
+const carouselImages = [
+  {
+    alt: 'Image 1',
+    src: '/src/images/events/jsconf-chile/jsconf-dia-2-evento-carousel-1.png'
+  },
+  {
+    alt: 'Image 2',
+    src: '/src/images/events/jsconf-chile/jsconf-dia-2-evento-carousel-2.png'
+  },
+  {
+    alt: 'Image 3',
+    src: '/src/images/events/jsconf-chile/jsconf-dia-2-evento-carousel-3.png'
+  }
+]
+
+export default carouselImages
