e9d4098677810e2c3a18d4e24d6adf612e3f28e0
Felix Icaza <fx.joliett17@gmail.com>
2025-03-07 19:48:13 -0600

    feat: add Nerdearla event page with carousel and content sections

---
+++ src/data/carousel-images-nerdearla.ts
@@ -0,0 +1,32 @@
+const carouselImages = [
+  {
+    alt: 'Image 1',
+    src: '/src/images/events/nerdearla/charla-nerdearla-carousel-1.png'
+  },
+  {
+    alt: 'Image 2',
+    src: '/src/images/events/nerdearla/charla-nerdearla-carousel-2.png'
+  },
+  {
+    alt: 'Image 3',
+    src: '/src/images/events/nerdearla/charla-nerdearla-carousel-3.png'
+  },
+  {
+    alt: 'Image 4',
+    src: '/src/images/events/nerdearla/charla-nerdearla-carousel-4.png'
+  },
+  {
+    alt: 'Image 5',
+    src: '/src/images/events/nerdearla/charla-nerdearla-carousel-5.png'
+  },
+  {
+    alt: 'Image 6',
+    src: '/src/images/events/nerdearla/charla-nerdearla-carousel-6.png'
+  },
+  {
+    alt: 'Image 7',
+    src: '/src/images/events/nerdearla/charla-nerdearla-carousel-7.png'
+  }
+]
+
+export default carouselImages
